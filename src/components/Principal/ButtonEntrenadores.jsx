9ef6e5de1f58cd68b7763dc1a8f0d5bf538cf713
Aldahir Fernandez <aldahirfernandez27@gmail.com>
2024-06-12 16:34:58 -0400

    Sin api

--- src/components/Principal/ButtonEntrenadores.jsx
+++ src/components/Principal/ButtonEntrenadores.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import './ButtonEntrenadores.css'
-import Entrenador1 from '../../assets/Entrenador1.png'
+import React, { useState, useEffect} from 'react';
+import './ButtonEntrenadores.css';
 
 const ButtonEntrenadores = () => {
+
+  const [entrenadores, setEntrenadores] = useState([]);
+
+  useEffect(() => {
+    // Reemplaza la URL con la de tu API
+    fetch('https://661038830640280f219c9b3d.mockapi.io/api/v2/users')
+      .then(response => response.json())
+      .then(data => setEntrenadores(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className='entre-container'>
-        <div className="entre-button">
-            <img src={Entrenador1} alt="Profile" className="profile-image" />
-        </div>
-        <p className="entre-name">Joaquin Pheonix</p> 
-    </div>
     
+    <div className='entre-container'>
+      {entrenadores.map(entrenador => (
+        <div key={entrenador.id} className="entre-button">
+          <img src={entrenador.avatar} alt={entrenador.name} className="profile-image" />
+          <p className="entre-name">{entrenador.name}</p>
+        </div>
+      ))}
+    </div>
     
   )
 }
