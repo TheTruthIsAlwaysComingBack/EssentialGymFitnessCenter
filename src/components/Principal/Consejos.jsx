9935dcf80c7e6251687913d7155b26a34239bf15
Aldahir Fernandez <aldahirfernandez27@gmail.com>
2024-06-12 00:47:40 -0400

    Version2 Home

--- src/components/Principal/Consejos.jsx
+++ src/components/Principal/Consejos.jsx
@@ -9,7 +9,7 @@ const Consejos = () => {
         <img src={Imagen} alt="Consejo del día" />
         <div className='adv-overlay'>
             <h2>CONSEJO DEL DÍA!</h2>
-            <p>Escucha a tu cuerpo, se constante y no te compares con los demas. Cada  personas tiene su propio ritmo y progreso. Celebra tus propios logros!</p>
+            <p>Escucha a tu cuerpo, se constante y no te compares con los demas. Cada  personas tiene su propio ritmo y progreso. Celebra tus propios logros!!</p>
         </div>
     </div>
   )
