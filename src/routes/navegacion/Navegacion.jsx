13cbbf9522ea9556f60b36e648871214a950db43
joseangel74 <mamanipinto.8470077@gmail.com>
2024-06-13 11:23:59 -0400

    avance1

--- src/routes/navegacion/Navegacion.jsx
+++ src/routes/navegacion/Navegacion.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../login/Logueo";
+import Desafios from "../Desafios/Desafios";
+import Rutina from "../Rutina/Rutina";
 
 const Navegacion = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/desafios" element={<Desafios />} />
+        <Route path="/rutina" element={<Rutina />} />
       </Routes>
     </Router>
   );
